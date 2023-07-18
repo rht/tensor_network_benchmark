9b5162db4657954d922ea331093915f5affdda11
rht <rhtbot@protonmail.com>
2023-07-18 05:40:01 -0400

    Move run_multiple_methods to common_tn.py

--- common_tn.py
+++ common_tn.py
@@ -232,3 +232,53 @@ def run_with_cusv(circuit):
     print("Elapsed cusv", elapsed)
     monitor.stop()
     return elapsed
+
+
+def run_multiple_methods(
+    circuit,
+    qubits,
+    index=0,
+    enable_oe=False,
+    enable_ctg=False,
+    enable_cutn=False,
+    enable_cusv=False,
+    enable_mps=False,
+):
+    output = {}
+    # pauli_string = {qubits[0]: "Z", qubits[1]: "Z"}
+    idx = 10 if len(qubits) > 10 else 0
+    pauli_string = {qubits[idx]: "Z"}
+
+    repeat_count = 2 if index == 0 else 1
+    if enable_oe:
+        print("Running with opt_einsum")
+        # Run twice in the beginning to warm the GPU.
+        for i in range(repeat_count):
+            out, elapsed = run_with_oe(circuit, pauli_string)
+        print("Output opt_einsum", out)
+        output["opt_einsum"] = elapsed
+
+    if enable_ctg:
+        print("Running with ctg")
+        # Run twice in the beginning to warm the GPU.
+        for i in range(repeat_count):
+            out, elapsed = run_with_ctg(circuit, pauli_string)
+        print("Output ctg", out)
+        output["ctg"] = elapsed
+
+    if enable_cutn:
+        print("Running with cutn")
+        out, elapsed = run_with_cutn(circuit, pauli_string)
+        print("Output cutn", out)
+        output["cutn"] = elapsed
+
+    # cusv
+    if enable_cusv:
+        print("Running with cusv")
+        elapsed = run_with_cusv(circuit)
+        output["cusv"] = elapsed
+
+    if enable_mps:
+        elapsed = run_with_mps(circuit)
+        output["mps"] = elapsed
+    return output
--- main.py
+++ main.py
@@ -6,64 +6,10 @@ from cuquantum import cutensornet as cutn
 import common_tn
 import circuits
 
-np.random.seed(1)
-
-
 np.random.seed(42)
 
-
-def run_multiple_methods(
-    circuit,
-    qubits,
-    index=0,
-    enable_oe=False,
-    enable_ctg=False,
-    enable_cutn=False,
-    enable_cusv=False,
-    enable_mps=False,
-):
-    output = {}
-    # pauli_string = {qubits[0]: "Z", qubits[1]: "Z"}
-    idx = 10 if len(qubits) > 10 else 0
-    pauli_string = {qubits[idx]: "Z"}
-
-    repeat_count = 2 if index == 0 else 1
-    if enable_oe:
-        print("Running with opt_einsum")
-        # Run twice in the beginning to warm the GPU.
-        for i in range(repeat_count):
-            out, elapsed = common_tn.run_with_oe(circuit, pauli_string)
-        print("Output opt_einsum", out)
-        output["opt_einsum"] = elapsed
-
-    if enable_ctg:
-        print("Running with ctg")
-        # Run twice in the beginning to warm the GPU.
-        for i in range(repeat_count):
-            out, elapsed = common_tn.run_with_ctg(circuit, pauli_string)
-        print("Output ctg", out)
-        output["ctg"] = elapsed
-
-    if enable_cutn:
-        print("Running with cutn")
-        out, elapsed = common_tn.run_with_cutn(circuit, pauli_string)
-        print("Output cutn", out)
-        output["cutn"] = elapsed
-
-    # cusv
-    if enable_cusv:
-        print("Running with cusv")
-        elapsed = common_tn.run_with_cusv(circuit)
-        output["cusv"] = elapsed
-
-    if enable_mps:
-        elapsed = common_tn.run_with_mps(circuit)
-        output["mps"] = elapsed
-    return output
-
-
 # import initialize_rqc
-# run_multiple_methods(initialize_rqc.qc, initialize_rqc.qc.qubits)
+# common_tn.run_multiple_methods(initialize_rqc.qc, initialize_rqc.qc.qubits)
 # exit()
 
 
@@ -100,7 +46,7 @@ def run_exp(exp_name):
             ansatz, qubits = circuits.qaoa_ansatz_with_cost_included(num_qubits)
         else:
             ansatz, qubits = circuits.qaoa_ansatz_with_cost_included(num_qubits)
-        output = run_multiple_methods(
+        output = common_tn.run_multiple_methods(
             ansatz,
             qubits,
             index=i,
